98557fa4e3141770d73d6b6b0a4c419615a1c82f
AdriHir <hirtzadrien@gmail.com>
2025-03-11 18:03:53 +0100

    change title AppBar

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product Manager"),
+        title: Text("Mon grenier"),
         centerTitle: true,
       ),
       body: Center(
